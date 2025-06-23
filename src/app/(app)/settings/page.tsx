8e02e1957dfd0699636bfad8ce1aa908f32ecf73
LautaroLD <duranlautarogabriel@gmail.com>
2025-06-22 21:04:05 -0300

    refactor: agregar componentes de página en desarrollo para las secciones de ayuda y configuración

---
+++ src/app/(app)/settings/page.tsx
@@ -0,0 +1,10 @@
+import UnderDevelopment from '@/components/UnderDevelopment'
+import React from 'react'
+
+export default function page() {
+  return (
+    <article className=' h-full'>
+      <UnderDevelopment />
+    </article>
+  )
+}
