0db1bf9f3b4258e29b2f36c9c10b7277a93d6fb9
LautaroLD <duranlautarogabriel@gmail.com>
2025-07-16 19:52:31 -0300

    refactor: agregar opciones predeterminadas para la configuración de QueryClient en QueryProvider

--- src/context/QueryProvider.tsx
+++ src/context/QueryProvider.tsx
@@ -1,7 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient()
-
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchInterval: false,
+      refetchIntervalInBackground: false,
+    },
+  },
+})
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
