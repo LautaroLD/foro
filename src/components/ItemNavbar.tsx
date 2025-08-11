fe8e7136fa99bfc2a8baae5726acf8b3ea8d072b
LautaroLD <duranlautarogabriel@gmail.com>
2025-08-11 02:24:13 -0300

    fix: agregar margen inferior al elemento de navegación en ItemNavbar

--- src/components/ItemNavbar.tsx
+++ src/components/ItemNavbar.tsx
@@ -12,7 +12,7 @@ export default function ItemNavbar({
     <li key={item.label}>
       <Link
         className={
-          'p-3 w-full hover:bg-[#b94d25] flex items-center gap-2 rounded-lg' +
+          'p-3 w-full hover:bg-[#b94d25] flex items-center gap-2 rounded-lg my-1' +
           (path === item.url ? ' bg-[#b94d25]' : '')
         }
         href={item.url}
